19c1f9f32894a89849c7b6bf4e6ad9dc8b6eba69
Kyle Altendorf <sda@fstab.net>
2020-11-24 18:59:19 -0500

    try symlinking libxcb-util as .0

---
+++ symlink_libxcb_util.py
@@ -0,0 +1,23 @@
+import pathlib
+import subprocess
+
+
+completed_process = subprocess.run(
+    ['ldconfig', '-p'],
+    check=True,
+    stdout=subprocess.PIPE,
+    encoding='utf-8',
+)
+
+print(completed_process.stdout)
+
+[xcb_util_line] = [
+    line
+    for line in completed_process.stdout.splitlines()
+    if 'libxcb-util' in line
+]
+
+existing_xcb_util_path = pathlib.Path(xcb_util_line.split('=>')[1])
+new = existing_xcb_util_path.with_suffix('0')
+new.symlink_to(existing)
+

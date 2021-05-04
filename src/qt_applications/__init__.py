07d4a5227126147c86ffd8b8f425961322bff383
Kyle Altendorf <sda@fstab.net>
2021-05-04 14:22:17 -0400

    left behind

--- src/qt_applications/__init__.py
+++ src/qt_applications/__init__.py
@@ -1,19 +1,20 @@
-import importlib
 import pathlib
-import pkg_resources
-
-major = int(pkg_resources.get_distribution(__name__).version.partition(".")[0])
 
 
-m = {
-    "pyqt_tools": "pyqt{major}_tools".format(major=major),
-    "pyqt_plugins": "pyqt{major}_plugins".format(major=major),
-    "qt_tools": "qt{major}_tools".format(major=major),
-    "qt_applications": "qt{major}_applications".format(major=major),
-}
-
-
+# TODO: CAMPid 0970432108721340872130742130870874321
 def import_it(*segments):
+    import importlib
+    import pkg_resources
+
+    major = int(pkg_resources.get_distribution(__name__).version.partition(".")[0])
+
+    m = {
+        "pyqt_tools": "pyqt{major}_tools".format(major=major),
+        "pyqt_plugins": "pyqt{major}_plugins".format(major=major),
+        "qt_tools": "qt{major}_tools".format(major=major),
+        "qt_applications": "qt{major}_applications".format(major=major),
+    }
+
     majored = [m[segments[0]], *segments[1:]]
     return importlib.import_module(".".join(majored))
 

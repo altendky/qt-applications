82e463b305a958a46f5a7ed830f1f9863f6cf3c8
Kyle Altendorf <sda@fstab.net>
2021-10-12 21:54:57 -0400

    accomodate macos 6.1.2+ path change

--- build.py
+++ build.py
@@ -605,7 +605,15 @@ class QtPaths:
             platform_,
     ):
         compiler_path = base / version / compiler
-        bin_path = compiler_path / 'bin'
+
+        if (
+                tuple(int(s) for s in version.split('.')) >= (6, 1, 2)
+                and platform_ == 'darwin'
+        ):
+            bin_path = compiler_path / 'macos' / 'bin'
+        else:
+            bin_path = compiler_path / 'bin'
+
         lib_path = compiler_path / 'lib'
         translation_path = compiler_path / 'translations'
 
